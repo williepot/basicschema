0ad125159f1c2d814ed6c7a0bef91c5271a2890e
Willie Potgieter <willem.potgieter@ttekglobal.com>
2018-10-19 10:18:19 +0700

    Completed module v. 1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-// Write your ES6 code in this file and run in console to test:
-// Yarn:  yarn dev
-// NPM:   npm run dev
+class BasicSchema {
+  constructor (state) {
+    this.state = state
+  }
 
-console.log("Thanks for cloning!")
+  // Returns an array of all field names
+  get getFieldNames () {
+    let temp = Object.entries(this.state)
+    let arr = []
+    for (var i in temp) {
+      arr.push(temp[i][0])
+    }
+    return arr
+  }
+
+  // Returns the schema object
+  get model () {
+    return this.state
+  }
+
+  // Sets the 'value' field of all field objects to 'null'
+  reset () {
+    for (var field in this.state) {
+      this.state[field].value = null
+    }
+  }
+
+  // Returns the value of a specific field
+  getField (field) {
+    return this.state[field].value
+  }
+
+  // Sets the value of a specific field
+  setField (field, value) {
+    this.state[field].value = value
+  }
+}
+
+module.exports = BasicSchema
